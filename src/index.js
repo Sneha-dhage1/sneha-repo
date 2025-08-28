908dea221cc29773f9f385f977d474ac37c265a2
sneha-dhage1 <dhagesneha9@gmail.com>
2025-08-28 12:28:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => {
+  return <h1>Hello from My React App 🚀</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
